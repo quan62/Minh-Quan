b3e3cbf010b44f7649bdd6fc6cf1713b0cd351ad
quan62 <quan622009@gmail.com>
2024-12-15 16:42:55 +0700

    Add files via upload

---
+++ Operator/src/basic/dev/MainApp.java
@@ -0,0 +1,14 @@
+package basic.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int a = 10;
+        int b, c, d;
+        b = c = d = a;
+        String str = a < b ? "a lon hon b" : "b lon hon a";
+        System.out.println(str);
+	}
+
+}
